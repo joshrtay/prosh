ec811d6fde19714635ba2b8d61f6b79a922fefc8
Josh Taylor <joshrtay@gmail.com>
2016-05-16 19:10:35 -0700

    take cwd as option

--- test/index.js
+++ test/index.js
@@ -9,15 +9,18 @@ const prosh = require('../')
  * Tests
  */
 
+const capture = true
+
 test('should echo and consume', (t) => {
-  prosh('echo "hello world"', true).then((stdout) => {
+  prosh('echo "hello world"', {capture}).then((stdout) => {
+    console.log('stdout', stdout)
     t.equal(stdout, 'hello world\n')
     t.end()
   })
 })
 
 test('should multi line echo and consume', (t) => {
-  prosh('echo "hello world"\necho "hello world"', true).then((stdout) => {
+  prosh('echo "hello world"\necho "hello world"', {capture}).then((stdout) => {
     t.equal(stdout, 'hello world\nhello world\n')
     t.end()
   })
