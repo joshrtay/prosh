faa9b7ac62e59df254457f9b7ace7ca92a12c97e
Josh Taylor <josh@joshs-mbp.lan>
2017-09-11 21:33:08 -0700

    change api and return value

--- test/index.js
+++ test/index.js
@@ -12,30 +12,23 @@ const prosh = require('../')
 const capture = true
 
 test('should echo and consume', (t) => {
-  prosh('echo "hello world"', {capture}).then((stdout) => {
-    t.equal(stdout, 'hello world\n')
+  prosh('echo "hello world"').then(({output}) => {
+    t.equal(output, 'hello world\n')
     t.end()
   }).catch(err => console.log('error', err))
 })
 
 test('should multi line echo and consume', (t) => {
-  prosh('echo "hello world"\necho "hello world"', {capture}).then((stdout) => {
-    t.equal(stdout, 'hello world\nhello world\n')
-    t.end()
-  })
-})
-
-test('should echo', (t) => {
-  prosh('echo "hello world"').then(stdout => {
-    t.equal(stdout, '')
+  prosh('echo "hello world"\necho "hello world"').then(({output}) => {
+    t.equal(output, 'hello world\nhello world\n')
     t.end()
   })
 })
 
 test('should get environment variables', (t) => {
   process.env.SECRET = 'hello-world'
-  prosh('echo $SECRET', {capture}).then(stdout => {
-    t.equal(stdout, 'hello-world\n')
+  prosh('echo $SECRET').then(({output}) => {
+    t.equal(output, 'hello-world\n')
     t.end()
   })
 })
